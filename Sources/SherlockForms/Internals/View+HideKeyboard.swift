6200e0e6fec60b38683d0571e18bc26241574563
Yasuhiro Inami <inamiy@gmail.com>
2022-02-17 22:49:01 +0900

    [Forms] Add `touchesEnd` override to hide keyboard

--- Sources/SherlockForms/Internals/View+HideKeyboard.swift
+++ Sources/SherlockForms/Internals/View+HideKeyboard.swift
@@ -1,11 +1,25 @@
 import SwiftUI
 
-extension View
+@MainActor
+func hideKeyboard()
 {
-    @MainActor
-    func hideKeyboard()
+    let resign = #selector(UIResponder.resignFirstResponder)
+    UIApplication.shared.sendAction(resign, to: nil, from: nil, for: nil)
+}
+
+extension UIView
+{
+    // NOTE:
+    // `UIView.touchesEnded` extension works better than `form.onTapGesture`
+    // which causes some form UI to stop working.
+    // cf. https://developer.apple.com/forums/thread/127196
+    open override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
     {
-        let resign = #selector(UIResponder.resignFirstResponder)
-        UIApplication.shared.sendAction(resign, to: nil, from: nil, for: nil)
+        super.touchesEnded(touches, with: event)
+
+        let className = "\(self)"
+        if className.contains("CellHostingView") && className.contains("SwiftUI") {
+            hideKeyboard()
+        }
     }
 }
